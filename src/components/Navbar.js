ab3f653bd302dd752cac6a61936ad1111beb2102
Aasiacoder <aasia3017@gmail.com>
2024-10-14 20:10:45 +0530

    final one

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,10 @@ function Navbar() {
         <nav>
             <h1>Perfumy</h1>
             <ul>
-                <li> Home |</li>
-                <li> Products |</li>
-                <li> About |</li>
-                <li> Contact </li>
+                <li>Home |</li>
+                <li>Products |</li>
+                <li>About |</li>
+                <li>Contact </li>
             </ul>
         </nav>
     )
